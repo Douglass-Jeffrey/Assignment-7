9544969156ec7ca89ec6650e659a4b9c056451d8
Ubuntu <ubuntu@ip-172-31-90-67.ec2.internal>
2019-12-10 00:30:31 +0000

    commit 1

---
+++ numberFinder.cpp
@@ -0,0 +1,52 @@
+// Copyright (c) 2019 St. Mother Teresa HS All rights reserved.
+//
+// Created by: Douglass Jeffrey
+// Created on: Nov 2019
+// This program finds a number in a random array
+
+#include <iostream>
+#include <cstdlib>
+#include <string>
+#include <ctime>
+
+
+int main() {
+    // This program finds numbers in a array
+    // Variables
+    srand(time(NULL));
+    int numberArray[50];
+    int seed;
+    int repeater = 0;
+    int randomnumbers = 0;
+    std::string userNumber;
+    int userNumberInt;
+    int checkerVal = 0;
+
+    // Inputs
+    for (repeater = 0; repeater < 50 ; repeater++) {
+        randomnumbers = (rand() % 100) + 1;
+        std::cout << "Generated number " << repeater + 1 << " is " <<
+                                            randomnumbers << std::endl;
+        numberArray[repeater] = randomnumbers;
+    }
+    std::cout << "Please enter the number you wish to search for: ";
+    std::cin >> userNumber;
+    // process and output
+    try {
+        userNumberInt = std::stoi(userNumber);
+        for (repeater = 0; repeater < 50; repeater++) {
+            if (userNumberInt == numberArray[repeater]) {
+                std::cout << "The number " << userNumberInt
+                          << " is located on the list at the "
+                          << repeater + 1 << "th place" << std::endl;
+                checkerVal = 1;
+            }
+        }
+    } catch (...) {
+        checkerVal = 1;
+        std::cout << "Please input only proper integer numbers" << std::endl;
+    }
+    if (checkerVal == 0) {
+        std::cout << "Your number was not on the list" << std::endl;
+    }
+}
